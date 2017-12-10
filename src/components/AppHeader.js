235ecd90b08ade0253c63af099124f966407a0b9
amandeepmittal <amandeepmittal@live.com>
2017-12-10 13:31:35 +0530

    remove unnecessary styles

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -27,18 +27,4 @@ const AppHeader = () => {
   );
 };
 
-const style = {
-  viewStyle: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 60,
-    paddingTop: 15,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    elevation: 2,
-    position: "relative"
-  }
-};
-
 export default AppHeader;
