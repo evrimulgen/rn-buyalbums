e1ae3b7b59c6a9d21a9619487fc4a2f74665c6d4
amandeepmittal <amandeepmittal@live.com>
2017-12-10 14:31:17 +0530

    complete

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Linking } from "react-native";
+import { Card, Button } from "react-native-elements";
 
 const AlbumDetail = ({ album }) => {
-  const { title } = album;
+  const { title, artist, image, url } = album;
 
   return (
     <View>
-      <Text>{title}</Text>
+      <Card
+        title={title}
+        containerStyle={{
+          marginBottom: 5,
+          position: "relative"
+        }}
+        image={{ uri: image }}
+      >
+        <Text style={{ textAlign: "center", marginBottom: 10 }}>
+          Album By {artist}
+        </Text>
+        <Button
+          icon={{ name: "shopping-cart" }}
+          backgroundColor="#691A99"
+          buttonStyle={{
+            borderRadius: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            marginBottom: 0
+          }}
+          title="Buy"
+          onPress={() => Linking.openURL(url)}
+        />
+      </Card>
     </View>
   );
 };
